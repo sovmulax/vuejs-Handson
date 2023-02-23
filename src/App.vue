b62d1833002be8bd4115602c60c317e681ade5d0
sovmulax <soumailaevane@gmail.com>
2023-02-23 19:37:17 +0000

    baseline 😎

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import Card from './components/Card.vue'
+  import Modal from './components/Modal.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Notes</h1>
+    <button class="vert">+</button>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  .vert{
+    width: 30px;
+    height: 30px;
+    border-radius: 100%;
+    border: 0px;
+    background-color: hsla(160, 100%, 37%, 1);
+    color: #fff;
+    /* font-size: 1. ; */
+    padding: 1,5rem;
+  }
 
-@media (min-width: 1024px) {
-  header {
+  div{
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: center;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
